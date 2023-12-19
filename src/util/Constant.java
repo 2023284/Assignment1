8b3baf1b0feb84c0451b985c75b6f9f492000a46
TaimoorNawab <2023364@student.cct.ie>
2023-12-19 06:16:59 +0000

    Create new classes name inputvalidator and constant
    create a class constant and add DB connection

---
+++ src/util/Constant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Dell
+ */
+public class Constant {
+     public static final String DB_URL = "jdbc:mysql://localhost:3306/tax_calculator";
+    public static final String DB_USER = "ooc2023";
+    public static final String DB_PASSWORD = "ooc2023";
+}
